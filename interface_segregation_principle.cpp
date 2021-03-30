1993999525995b3eac972031c18fbaf1eba9f31c
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2021-03-30 18:05:49 +1030

    comment improvments

--- interface_segregation_principle.cpp
+++ interface_segregation_principle.cpp
@@ -2,7 +2,7 @@
 #include "helper_functions.hpp"
 
 //The goal of the interface segregation principle is to not create interfaces that are too large
-//Large interfaces cause cause the implements to perhaps implement too much
+//Large interfaces cause the inherited structures to implement too much
 
 struct Document
 {
@@ -10,7 +10,7 @@ struct Document
 };
 
 //Observe the following interface
-//You want a machine that can scan, print and fax documents. So yoy create an interface for each functionality
+//You want a machine that can scan, print and fax documents. So you create an interface for each functionality
 //The problem with the interface below is that now you have too many implementations to handle for one interface
 struct IMachine{
     virtual void print(Document& doc) = 0;
@@ -18,7 +18,7 @@ struct IMachine{
     virtual void fax(Document& doc) = 0;
 };
 
-//lets say you just wanted a printer. However now you have to implement the scan and fax functionality as well, even thouhg it doesnt make sence too 
+//lets say you just wanted a printer. However now you have to implement the scan and fax functionality as well, even though it doesnt make sense too 
 struct MPrinter : IMachine{
     void print(Document& doc) override{
 
@@ -33,7 +33,7 @@ struct MPrinter : IMachine{
     }
 };
 
-//A better solution, is to create seperate interfaces, and combining with the single responsibility principle to create simple, understandble classes
+//A better solution, is to create separate interfaces, and combining with the single responsibility principle to create simple, understandble templates
 
 struct Printer{
     virtual void print(Document &doc) = 0;
@@ -47,7 +47,7 @@ struct Fax{
     virtual void fax(Document &doc) = 0;
 };
 
-//Now there is no need for methods to be left empty or too throw exceptions in interfaces. Nothing is redundant.
+//Now there is no need for methods to be left empty or too throw exceptions in inherited structures. Nothing is redundant.
 int main(){
     
     return 0;
