d530e7e198ddd1a2b272d834ebd5fc15bc4b7063
Basil <62039344+BasilDimopoulos@users.noreply.github.com>
2021-03-31 21:24:05 +1030

    Dependency Inversion Principle Setup

---
+++ dependency_inversion_principle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "helper_functions.hpp"
+
+//The dependency inversion principle specifies the best way to form dependencies between two objects,
+// IDEA 1: High level modules shuold not depend on low-level modules
+//         Both should depend on abstractions
+// IDEA 2: Abstractions should not depend on details
+// Details should depend on abstractions
+
+int main()
+{
+
+    return 0;
+}
