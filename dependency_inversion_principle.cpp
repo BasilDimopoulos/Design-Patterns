7bf57b7f7399de98be7d3fe38721a790c473aaf7
Basil <62039344+BasilDimopoulos@users.noreply.github.com>
2021-04-01 13:08:47 +1030

    updates

--- dependency_inversion_principle.cpp
+++ dependency_inversion_principle.cpp
@@ -20,8 +20,13 @@ struct Person
     string name;
 };
 
+struct RelationshipBrowser
+{
+    virtual vector<Person> find_all_children_of(const string &name) = 0;
+};
+
 //We may want some sort of low level structure for storing relationships
-struct Relationships
+struct Relationships : RelationshipBrowser
 {
     //We can make a tuple that stores two people and their relationship
     vector<tuple<Person, Relationship, Person>> relations;
@@ -33,6 +38,36 @@ struct Relationships
         relations.push_back({child, Relationship::child, parent});
         //Remember we need to map both the parents relatinship to the child, and the childs relationship to the parent
     }
+
+    vector<Person> find_all_children_of(const string &name) override
+    {
+        vector<Person> result;
+        for (auto &&[first, rel, second] : relations)
+        {
+            if (first.name == name && rel == Relationship::parent)
+            {
+                result.push_back(second);
+            }
+        }
+        return result;
+    }
+};
+
+struct Research //high level
+{
+    Research(Relationships &relationships)
+    {
+        // this is bad, a high level module should not depend on a low level module
+        // if the relationshions storage module was to change or the relations module was made private, the high level code would be completly broken
+        auto &relations = relationships.relations;
+        for (auto &&[first, rel, second] : relations)
+        {
+            if (first.name == "John" && rel == Relationship::parent)
+            {
+                cout << "John has a child called" << second.name << endl;
+            }
+        }
+    }
 };
 
 int main()
@@ -42,5 +77,7 @@ int main()
     Relationships relationships;
     relationships.add_parent_and_child(parent, child);
     relationships.add_parent_and_child(parent, child2);
+
+    Research _(relationships);
     return 0;
 }
