f82f54394f1591b24293a20a0b485d3f83ba2736
Basil <62039344+BasilDimopoulos@users.noreply.github.com>
2021-04-03 15:37:38 +1030

    builder setup

---
+++ builder.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "helper_functions.hpp"
+
+// Lets say we wanted a server that served html elements
+// We need a scalable object oriented approach to handling all the different types of elements and tags
+
+int main()
+{
+
+    return 0;
+}
