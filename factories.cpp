16711dc7e28be49680c8169f94c55d22f89007d7
Basil <62039344+BasilDimopoulos@users.noreply.github.com>
2021-04-09 12:03:36 +0930

    Factory Method

--- factories.cpp
+++ factories.cpp
@@ -12,35 +12,54 @@ enum class PointType
     polar
 };
 
-struct Point
+//struct Point
+//{
+//lets say you have a point and the constructor initialises the points on the cartiesean plane
+float x, y;
+//Point(float x, float y) : x(x), y(y) {}
+
+//then lets say something changes and you know want to initialise it with polar coordinates. So you make a new constructor
+//Point(float rho, float theta) {}
+//Unfortunately you cannot make two constructors
+
+//so you make an enum class specifying the type of point and then use that for the initialisation
+//Unfortunately the below code is a bit confusing. You dont know what a and b are refering to, whether they are point x&y in the cartesean plane or rho&theta in the polar plane
+//     Point(float a, float b, PointType type = PointType::cartesian)
+//     {
+//         if (type == PointType::cartesian)
+//         {
+//             x = a;
+//             y = b;
+//         }
+//         else
+//         {
+//             x = a * cos(b);
+//             y = a * sin(b);
+//         }
+//     }
+// };
+
+//A better approach would be using encapsulation to hide the constructor and then utilizing functions inside to call the constructer. However by doing so you will not be able to call the constructor on creation as you normally would.
+class Point
 {
-    //lets say you have a point and the constructor initialises the points on the cartiesean plane
+    Point(float x, float y) : x(y), y(y) {}
+
+public:
     float x, y;
-    //Point(float x, float y) : x(x), y(y) {}
-
-    //then lets say something changes and you know want to initialise it with polar coordinates. So you make a new constructor
-    //Point(float rho, float theta) {}
-    //Unfortunately you cannot make two constructors
-
-    //so you make an enum class specifying the type of point and then use that for the initialisation
-    //Unfortunatly the below code is a bit confusing. You dont know what a and b are refering to, whether they are point x&y in the cartesean plane or rho&theta in the polar plane
-    Point(float a, float b, PointType type = PointType::cartesian)
+    static Point NewCartesian(float x, float y)
     {
-        if (type == PointType::cartesian)
-        {
-            x = a;
-            y = b;
-        }
-        else
-        {
-            x = a * cos(b);
-            y = a * sin(b);
-        }
+        return {x, y};
+    }
+
+    static Point NewPolar(float r, float theta)
+    {
+        return {r * cos(theta), r * sin(theta)};
     }
 };
 
 int main()
 {
-
+    auto p = Point::NewPolar(5, 3.42);
+    cout << p.x << endl;
     return 0;
 }
